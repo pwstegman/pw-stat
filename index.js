d778519d462f0fe0f2ab511f0d9fb29831325fc8
Pierce Stegman <pwstegman@gmail.com>
2018-09-26 21:33:49 -0500

    Added strict mode support

--- index.js
+++ index.js
@@ -4,41 +4,41 @@
  */
 function cov(x) {
 	// Useful variables
-	N = x.length;
+	let N = x.length;
 
 	// Step 1: Find expected value for each variable (columns are variables, rows are samples)
-	E = [];
-	for (var c = 0; c < x[0].length; c++) {
-		u = 0;
-		for (var r = 0; r < N; r++) {
+	let E = [];
+	for (let c = 0; c < x[0].length; c++) {
+		let u = 0;
+		for (let r = 0; r < N; r++) {
 			u += x[r][c];
 		}
 		E.push(u / N);
 	}
 
 	// Step 2: Center each variable at 0
-	var centered = [];
-	for (var r = 0; r < N; r++) {
+	let centered = [];
+	for (let r = 0; r < N; r++) {
 		centered.push([]);
-		for (var c = 0; c < x[0].length; c++) {
+		for (let c = 0; c < x[0].length; c++) {
 			centered[r].push(x[r][c] - E[c]);
 		}
 	}
 
 	// Step 3: Calculate covariance between each possible combination of variables
-	S = [];
+	let S = [];
 
-	for (var i = 0; i < x[0].length; i++) {
+	for (let i = 0; i < x[0].length; i++) {
 		S.push([]);
 	}
 
-	for (var c1 = 0; c1 < x[0].length; c1++) {
+	for (let c1 = 0; c1 < x[0].length; c1++) {
 		// Doing c2 = c1; because, for example, cov(1,2) = cov(2, 1)
 		// so no need to recalculate
-		for (var c2 = c1; c2 < x[0].length; c2++) {
+		for (let c2 = c1; c2 < x[0].length; c2++) {
 			// Solve cov(c1, c2) = average of elementwise products of each variable sample
-			var cov = 0;
-			for (var r = 0; r < N; r++) {
+			let cov = 0;
+			for (let r = 0; r < N; r++) {
 				cov += centered[r][c1] * centered[r][c2];
 			}
 			cov /= (N - 1); // N-1 for sample covariance, N for population. In this case, using sample covariance
@@ -55,19 +55,19 @@ function cov(x) {
  * @param {number[][]} x - Two-dimensional matrix.
  */
 function mean(x) {
-	var result = [];
+	let result = [];
 	
-	for(var c = 0; c<x[0].length; c++){
+	for(let c = 0; c<x[0].length; c++){
 		result.push(0);
 	}
 	
-	for (var r = 0; r < x.length; r++) {
-		for(var c = 0; c<x[r].length; c++){
+	for (let r = 0; r < x.length; r++) {
+		for(let c = 0; c<x[r].length; c++){
 			result[c] += x[r][c];
 		}
 	}
 	
-	for(var c = 0; c<x[0].length; c++){
+	for(let c = 0; c<x[0].length; c++){
 		result[c] /= x.length;
 	}
 	
